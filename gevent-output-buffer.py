51680e77785cbd7c97898e90e7744323c896f85e
lpaszke <lukasz.paszke@implix.com>
2019-06-06 12:43:17 +0200

    Update gevent-output-buffer.py

--- gevent-output-buffer.py
+++ gevent-output-buffer.py
@@ -1,1 +1,11 @@
 # Copyright (c) 2019, Łukasz Paszke. All rights reserved. see README for details.
+
+try:
+    import gevent
+except ImportError:
+    print 'Please install gevent and its dependencies'
+
+class GEventOutputBuffer(OutputBuffer):
+    def spawn_buffer_thread(spawn_function):
+        gevent.spawn(spawn_function)
+        
